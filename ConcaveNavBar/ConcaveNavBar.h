32934cb0953fd5cfa5fe84cc44140c40461fb2ad
MrTrent <show6time@gmail.com>
2022-08-16 15:18:46 +0300

    Initial project setup

---
+++ ConcaveNavBar/ConcaveNavBar.h
@@ -0,0 +1,18 @@
+//
+//  ConcaveNavBar.h
+//  ConcaveNavBar
+//
+//  Created by Andrew Zubarew on 16.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConcaveNavBar.
+FOUNDATION_EXPORT double ConcaveNavBarVersionNumber;
+
+//! Project version string for ConcaveNavBar.
+FOUNDATION_EXPORT const unsigned char ConcaveNavBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConcaveNavBar/PublicHeader.h>
+
+
